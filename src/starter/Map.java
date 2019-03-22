be0d138d38812fe68ac9202a3231f6a3f8e83c36
Richard <rickyrich0988@gmail.com>
2019-03-21 18:21:15 -0700

    Created obstacle class and level file.

--- src/starter/Map.java
+++ src/starter/Map.java
@@ -1,11 +1,17 @@
 package starter;
 
+import java.util.*;
 import java.io.*;
 
 public class Map {
 
-	private static final String FILENAME = "test.txt";
+	private static final String FILENAME = "level.txt";
 	
+	private ArrayList<Obstacle> Obstacles;
+	
+	public Map() {
+		Obstacles = new ArrayList<Obstacle>();
+	}
 	
 	
 }
