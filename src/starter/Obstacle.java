28beba189f5d4c9f416051566f8d4a2a08430843
Martin <m_zacarias@u.pacific.edu>
2019-03-24 17:41:13 -0700

    Added setters and getters

--- src/starter/Obstacle.java
+++ src/starter/Obstacle.java
@@ -1,17 +1,57 @@
 package starter;
 
 public class Obstacle {
-	Size size;
-	boolean movement;
-	Position position;
-	Velocity velocity;
-	boolean instantDeath;
+	private Size size;
+	private boolean movement;
+	private Position position;
+	private Velocity velocity;
+	private boolean instantDeath;
 	
 	public Obstacle(int width, int length, boolean move, int x, int y, int horizontal, int vertical, boolean death) {
-		size = new Size(width, length);
-		movement = move;
-		position = new Position(x, y);
-		velocity = new Velocity(horizontal, vertical);
-		instantDeath = death;
+		setSize(new Size(width, length));
+		setMovement(move);
+		setPosition(new Position(x, y));
+		setVelocity(new Velocity(horizontal, vertical));
+		setInstantDeath(death);
+	}
+
+	public boolean isInstantDeath() {
+		return instantDeath;
+	}
+
+	public void setInstantDeath(boolean instantDeath) {
+		this.instantDeath = instantDeath;
+	}
+
+	public Velocity getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(Velocity velocity) {
+		this.velocity = velocity;
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	public boolean isMovement() {
+		return movement;
+	}
+
+	public void setMovement(boolean movement) {
+		this.movement = movement;
+	}
+
+	public Size getSize() {
+		return size;
+	}
+
+	public void setSize(Size size) {
+		this.size = size;
 	}
 }
